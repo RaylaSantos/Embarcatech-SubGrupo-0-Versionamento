8a8c8606c9dfbd47b91df13f0cb15d9a2eeca4d8
DIEGO MICKAEL <diego.bme@hotmail.com>
2024-12-20 00:00:59 -0300

    Implementado a função de selecionar e unidade de entrada na função de converter comprimento.

--- conversor.c
+++ conversor.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 
 void comprimento();
@@ -60,42 +61,151 @@ int main(){
 }
 
 void comprimento(){
-    int escolha;
+    int escolha,escolha2;
     bool repete = true;
     double n;
-    printf("Entre com um valor em [m] para conversão: ");
-    scanf("%lf",&n);
     do{
+    printf("Qual unidade deseja converter? \n");
+    printf("[1] para quilometro [km]\n");
+    printf("[2] para metros [m] \n");
+    printf("[3] para centimetros [cm]\n");
+    printf("[4] para milimetros [mm]\n");
+    scanf("%d",&escolha2);
+    fflush(stdin);
+    printf("Entre com um valor em para ser convertido: ");
+    scanf("%lf",&n);
+    fflush(stdin);
     printf("Qual a unidade desejada? \n");
-    printf("[1] para km\n");
-    printf("[2] para cm\n");
-    printf("[3] para mm\n");
+    printf("[1] para quilometro [km]\n");
+    printf("[2] para metros [m]\n");
+    printf("[3] para centimetros [cm]\n");
+    printf("[4] para milimetros [mm]\n");
     printf("[Outro] Retornar ao principal.\n");
     scanf("%d",&escolha);
     fflush(stdin);
-    if(escolha>0||escolha<4){
-        repete=true;
-    }
-    switch (escolha)
-    {
-    case 1:
-        printf("km: %.3lf\n",n/1000);
-        printf("Entre com qualquer tecla para retornar.\n");
-        getchar();
-        break;
-    case 2:
-        printf("cm: %.1lf\n",n*100);
-        printf("Entre com qualquer tecla para retornar.\n");
-        getchar();
-        break;
-    case 3:
-        printf("mm: %.1lf\n",n*1000);
-        printf("Entre com qualquer tecla para retornar.\n");
-        getchar();
-        break;
-    default:
-        printf("Retornando ao menu inicial...\n");
-        repete = false;
+    switch(escolha2){
+        case 1: //Quilometro
+                    switch(escolha)
+                    {
+                    case 1:
+                        printf("%.3lfkm em km: %.3lf\n",n,n);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 2:
+                        printf("%.3lfkm em m: %.1lf\n",n,n*1000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 3:
+                        printf("%.3lfkm em cm: %.1lf\n",n,n*100000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 4:
+                        printf("%.3lfkm em mm: %.1lf\n",n,n*1000000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    default:
+                        printf("Saindo...\n");
+                        repete = false;
+                        break;
+                    }
+                    break;
+
+        case 2: //Metro
+                    switch(escolha)
+                    {
+                    case 1:
+                        printf("%.3lfm em km: %.3lf\n",n,n/1000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 2:
+                        printf("%.3lfm em m: %.1lf\n",n,n);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 3:
+                        printf("%.3lfm em cm: %.1lf\n",n,n*100);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 4:
+                        printf("%.3lfm em mm: %.1lf\n",n,n*1000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    default:
+                        printf("Saindo...\n");
+                        repete = false;
+                        break;
+                    }
+                    break;
+
+        case 3: //centimetro
+            switch(escolha)
+                    {
+                    case 1:
+                        printf("%.3lfcm em km: %.8f\n",n,n/100000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 2:
+                        printf("%.3lfcm em m: %.3lf\n",n,n/100);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 3:
+                        printf("%.3lfcm em cm: %.3lf\n",n,n);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 4:
+                        printf("%.3lfcm em mm: %.3lf\n",n,n*10);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    default:
+                        printf("Saindo...\n");
+                        repete = false;
+                        break;
+                    }
+                    break;
+
+        case 4: //milimetro
+            switch(escolha)
+                    {
+                    case 1:
+                        printf("%.3lfcm em km: %.8lf\n",n,n/1000000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 2:
+                        printf("%.3lfcm em m: %.4lf\n",n,n/1000);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 3:
+                        printf("%.3lfcm em cm: %.3lf\n",n,n/10);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    case 4:
+                        printf("%.3lfcm em mm: %.3lf\n",n);
+                        printf("Entre com qualquer tecla para retornar.\n");
+                        getchar();
+                        break;
+                    default:
+                        printf("Saindo...\n");
+                        repete = false;
+                        break;
+                    }
+                    break;
+        default:
+        printf("Escolha invalida...\n");
+        
         break;
     }
     }while(repete==true);
